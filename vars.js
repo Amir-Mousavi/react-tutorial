4c4f3f5a667e4b4a8f66b41d384bb60835442ada
Tutorial <tutorial@amir.fritz.box>
2019-02-09 11:48:50 +0100

    Variables in javascript https://youtu.be/xn09xsaii_4

---
+++ vars.js
@@ -0,0 +1,22 @@
+var name = "John";
+
+
+var name = "Emma";
+
+console.log('name', name);
+
+// let 
+// const
+
+ let person = "something";
+
+ person = "Amir";
+
+ console.log('person', person);
+
+ const number = 2;
+
+//  number = 3; 
+
+
+
