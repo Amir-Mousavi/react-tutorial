c3dce95b2223a9095e976695571ecbf808632c41
Amir <amir.mousavie@gmail.com>
2019-02-09 17:57:48 +0100

    loops in js

---
+++ loops.js
@@ -0,0 +1,12 @@
+
+for (let i=0; i < 10; i+=1) {
+    console.log('index: ', i);
+}
+
+let index = 10;
+
+while(index < 20) {
+    console.log('index in a while loop: ', index);
+
+    index += 1;
+}
